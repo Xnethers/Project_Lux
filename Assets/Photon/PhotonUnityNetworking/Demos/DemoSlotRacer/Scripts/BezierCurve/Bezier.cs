e9b1946cd148d4c4cad24baeae790b4164e84888
Xnethers <z44440000z@gmail.com>
2019-10-23 00:47:11 +0800

    Room for 1 1.Adela isAI勾選 2.GameManager->Plaer Prefab換上正太 # LobbyScene 1.增加正太 2.正太的參數變更 # 正太 1.正太普攻完成 2.添加正太prefab 3.增加正太用的腳本 # 愛德拉 1.修改DoubleGunController 2.右鍵bug修復

--- Assets/Photon/PhotonUnityNetworking/Demos/DemoSlotRacer/Scripts/BezierCurve/Bezier.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/DemoSlotRacer/Scripts/BezierCurve/Bezier.cs
@@ -11,46 +11,52 @@
 
 using UnityEngine;
 
-namespace Photon.Pun.Demo.SlotRacer.Utils
+
+public static class Bezier
 {
-	public static class Bezier
-	{
-		public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
-		{
-			t = Mathf.Clamp01(t);
-			float oneMinusT = 1f - t;
-			return
-				oneMinusT * oneMinusT * p0 +
-				2f * oneMinusT * t * p1 +
-				t * t * p2;
-		}
+    /// <summary> 
+    /// 根據T值，計算貝塞爾曲線上面相對應的點 
+    /// </summary> 
+    /// <param name="t"></param>T值 
+    /// <param name="p0"></param>起始點 
+    /// <param name="p1"></param>控制點 
+    /// <param name="p2"></param>目標點
 
-		public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
-		{
-			return
-				2f * (1f - t) * (p1 - p0) +
-				2f * t * (p2 - p1);
-		}
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            oneMinusT * oneMinusT * p0 +
+            2f * oneMinusT * t * p1 +
+            t * t * p2;
+    }
 
-		public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
-		{
-			t = Mathf.Clamp01(t);
-			float OneMinusT = 1f - t;
-			return
-				OneMinusT * OneMinusT * OneMinusT * p0 +
-				3f * OneMinusT * OneMinusT * t * p1 +
-				3f * OneMinusT * t * t * p2 +
-				t * t * t * p3;
-		}
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        return
+            2f * (1f - t) * (p1 - p0) +
+            2f * t * (p2 - p1);
+    }
 
-		public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
-		{
-			t = Mathf.Clamp01(t);
-			float oneMinusT = 1f - t;
-			return
-				3f * oneMinusT * oneMinusT * (p1 - p0) +
-				6f * oneMinusT * t * (p2 - p1) +
-				3f * t * t * (p3 - p2);
-		}
-	}
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float OneMinusT = 1f - t;
+        return
+            OneMinusT * OneMinusT * OneMinusT * p0 +
+            3f * OneMinusT * OneMinusT * t * p1 +
+            3f * OneMinusT * t * t * p2 +
+            t * t * t * p3;
+    }
+
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            3f * oneMinusT * oneMinusT * (p1 - p0) +
+            6f * oneMinusT * t * (p2 - p1) +
+            3f * t * t * (p3 - p2);
+    }
 }
--- Assets/Scripts/Mediator/Extra/PunchController.cs
+++ Assets/Scripts/Mediator/Extra/PunchController.cs
@@ -6,8 +6,12 @@ using Photon.Pun;
 using Photon.Realtime;
 
 //Adela
-public class DoubleGunController : ICareerController
+public class PunchController : ICareerController
 {
+    public GameObject[] Punches = new GameObject[2];
+    private Rigidbody[] Punches_rb = new Rigidbody[2];
+    public Transform[] CurvePoint = new Transform[2];
+    public Transform[] origLocPos = new Transform[2];
     public GameObject VFX_Adela_Q;
     public GameObject VFX_Adela_gunFire;
     private KICareer ki;
@@ -15,53 +19,93 @@ public class DoubleGunController : ICareerController
     new protected Transform[] muzzle = new Transform[3];
     private bool isleft = true;
 
+    [Header("===== Punch Settings =====")]
+    public bool ispunching;
+    public bool isreturn;
+    public float speed = 1.5f;
+    [SerializeField] private float returnTime;
+    private Vector3 targetPos;
+
     [Space(10)]
     [Header("===== Shooter Settings =====")]
     [SerializeField] float ThrowerPower;
-
-    [Header("===== Fill Settings =====")]
-    /* 彈匣 */
-    [SerializeField] GameObject Magazine;
-
-    [SerializeField] GameObject Obj_magazine;
-    public Transform[] magazinePos = new Transform[3];
-    //[0]==左,[1]==右,[2]==F
-    public bool isFill;
-    public int magazine = 40;
-
-    [Header("===== Force Settings =====")]
-    public GameObject[] Beam = new GameObject[2];
-    // public List<BodyCollider> APIV = new List<BodyCollider>(); //All Player In View 
-    // public List<Collider> allbodycollider;
-    // private BattleController[] allbc_obj;
-    // private LayerMask mask;
-    // public LayerMask ignoreMask;
-
-    // [HideInInspector]
-    // public Camera mainCamera;
-    // private MyTimer.STATE isRunState;
 
     public bool isForce;
     [Header("===== AudioClip Settings =====")]
     public AudioClip gunFire;
     public AudioClip repelAttack;
+
     void Start()
     {
-        muzzle[0] = transform.DeepFind("MuzzleL");
-        muzzle[1] = transform.DeepFind("MuzzleR");
-        muzzle[2] = transform.DeepFind("MuzzleF");
+        for (int i = 0; i < Punches.Length; i++)
+        {
+            Punches_rb[i] = Punches[i].GetComponent<Rigidbody>();
+            Punches[i].transform.position = origLocPos[i].position;
+            CurvePoint[i] = transform.DeepFind("curvepoint (" + i + ")");
+        }
+
         ac = GetComponent<ActorController>();
         ki = GetComponent<KICareer>();
-        foreach (var item in Beam)
-        { item.SetActive(false); }
         //StartCoroutine("Timer_Forcing");
 
     }
 
-
     void FixedUpdate()
     {
-        //target = FindTargetPoint().transform.position;
+        if (ispunching)
+        {
+            if (isreturn && IfReurn())
+            {
+                isleft = !isleft;
+                isreturn = ispunching = false;
+                returnTime = 0;
+                Punches_rb[0].transform.parent = transform;
+                Punches_rb[1].transform.parent = transform;
+            }
+            else if (!isreturn && returnTime < 1)
+            {
+                if (!isleft)
+                {
+                    Punches_rb[0].transform.parent = null;
+                    Punches_rb[0].position = Bezier.GetPoint(origLocPos[0].position, CurvePoint[0].position, targetPos, returnTime);
+                    returnTime += Time.deltaTime * speed;
+                }
+                else
+                {
+                    Punches_rb[1].transform.parent = null;
+                    Punches_rb[1].position = Bezier.GetPoint(origLocPos[1].position, CurvePoint[1].position, targetPos, returnTime);
+                    returnTime += Time.deltaTime * speed;
+                }
+            }
+            else if (isreturn && !IfReurn())
+            {
+                if (!isleft)
+                {
+                    Punches_rb[0].position = Bezier.GetPoint(targetPos, CurvePoint[0].position, origLocPos[0].position, returnTime);
+                    returnTime += Time.deltaTime * speed;
+                }
+                else
+                {
+                    Punches_rb[1].position = Bezier.GetPoint(targetPos, CurvePoint[1].position, origLocPos[1].position, returnTime);
+                    returnTime += Time.deltaTime * speed;
+                }
+            }
+            else if (!isreturn && returnTime >= 1)
+            {
+                if (!isleft)
+                {
+                    //targetPos = Punches_rb[0].position;
+                    photonView.RPC("RPC_Projectile", RpcTarget.All, Punches_rb[0].transform.position, RayAim(), 0f);
+                }
+                else
+                {
+                    //targetPos = Punches_rb[1].position;
+                    photonView.RPC("RPC_Projectile", RpcTarget.All, Punches_rb[1].transform.position, RayAim(), 0f);
+                }
+                PunchStartReturn();
+            }
+
+        }
     }
     void Update()
     {
@@ -84,7 +128,7 @@ public class DoubleGunController : ICareerController
         if (ac.pi.isLatent)
             return;
         //Debug.Log(skillML.atkTimer.elapsedTime);
-        if (ac.canAttack && !isFill)
+        if (ac.canAttack)
         {
             //canAttack限制狀態機行為
             if (ki.attackML)
@@ -136,7 +180,10 @@ public class DoubleGunController : ICareerController
 
         if (ki.auxiliaryMR)
         {
-            UseSkill(1, careerValue.FirstDamage);
+            if (ac.CheckState("forcing", "attack"))
+            {
+                UseSkill(1, careerValue.FirstDamage);
+            }
         }
 
 
@@ -158,26 +205,45 @@ public class DoubleGunController : ICareerController
         if (ki.attackML)
             isForce = false;
 
-        if (ki.R && !isFill)
+        if (ki.R)
         { photonView.RPC("RPC_SetTrigger", RpcTarget.All, "fillBullet"); }
 
+    }
+
+    bool IfReurn()
+    {
+        float d;
+        if (!isleft)
+        { d = Vector3.Distance(Punches_rb[0].position, origLocPos[0].position); }
+        else
+        { d = Vector3.Distance(Punches_rb[1].position, origLocPos[1].position); }
+        if (d > 0)
+        { return false; }
+        else
+        { return true; }
+    }
+
+    public void PunchStartReturn()
+    {
+        // pullPosition = weapon.position;
+        // weaponRb.Sleep();
+        // weaponRb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        // weaponRb.isKinematic = true;
+        isreturn = true;
+        returnTime = 0;
     }
 
 
     #region animator skill events
     public override void NormalAttack()
     {
-        CreateGunFire();
+        CreatePunchFire();
         if (!photonView.IsMine)
             return;
         else
         {
-            if (isleft)
-            { photonView.RPC("RPC_Projectile", RpcTarget.All, muzzle[0].position, RayAim(), ThrowerPower); }
-            else
-            { photonView.RPC("RPC_Projectile", RpcTarget.All, muzzle[1].position, RayAim(), ThrowerPower); }
-            magazine--;
-            isleft = !isleft;
+            ispunching = true;
+            targetPos = RayAim();
         }
     }
 
@@ -201,16 +267,13 @@ public class DoubleGunController : ICareerController
 
     public override void RushAttack()//Q
     {
-        CreateGunFire();
         if (!photonView.IsMine)
         { return; }
         photonView.RPC("RPC_Projectile", RpcTarget.All, transform.position, RayAim(), 0f);
-        magazine--;
     }
 
     public override void AirAttack()
     {
-        CreateGunFire();
         if (!photonView.IsMine)
         { return; }
         else
@@ -222,76 +285,57 @@ public class DoubleGunController : ICareerController
 
     public override void ForceAttack()//蓄力(0.7s)
     {
-        foreach (var item in Beam)
-        {
-            item.SetActive(true);
-            Projectile p = item.GetComponentInChildren<Projectile>();
-            p.Initialize(ac.am, 0, RayAim());
-        }
+
     }
 
     public void magazineOperation() //確認是否填彈
     {
-        if (magazine <= 0 && !isFill)
-        {
-            photonView.RPC("RPC_SetTrigger", RpcTarget.All, "fillBullet");
-            isFill = true;
-            ac.canAttack = false;
-        }
-        else
-        {
-            if (ac != null)
-                ac.canAttack = true;
-        }
     }
 
     public void BulletRecover() //填彈
     {
-        magazine = 10;
     }
     public void OnFillBulletExit() //填彈
     {
-        isFill = false;
-        ac.canAttack = true;
     }
 
-    public void CreatFallMagazine()
-    {
-        GameObject _magazine = (GameObject)Instantiate(Magazine, magazinePos[0].position, magazinePos[0].rotation);
-    }
+    // public void CreatFallMagazine()
+    // {
+    //     GameObject _magazine = (GameObject)Instantiate(Magazine, magazinePos[0].position, magazinePos[0].rotation);
+    // }
 
-    public void ChangeMagazinPos()
-    {
-        if (Obj_magazine.transform.parent == magazinePos[0])
-        {
-            Obj_magazine.SetActive(false);
-            Obj_magazine.transform.SetParent(magazinePos[1]);
-            Obj_magazine.transform.localPosition = Vector3.zero;
-            Obj_magazine.transform.localRotation = Quaternion.identity;
-        }
-        else if (Obj_magazine.transform.parent == magazinePos[1])
-        {
-            Obj_magazine.transform.SetParent(magazinePos[0]);
-            Obj_magazine.transform.localPosition = Vector3.zero;
-            Obj_magazine.transform.localRotation = Quaternion.identity;
-        }
-    }
+    // public void ChangeMagazinPos()
+    // {
+    //     if (Obj_magazine.transform.parent == magazinePos[0])
+    //     {
+    //         Obj_magazine.SetActive(false);
+    //         Obj_magazine.transform.SetParent(magazinePos[1]);
+    //         Obj_magazine.transform.localPosition = Vector3.zero;
+    //         Obj_magazine.transform.localRotation = Quaternion.identity;
+    //     }
+    //     else if (Obj_magazine.transform.parent == magazinePos[1])
+    //     {
+    //         Obj_magazine.transform.SetParent(magazinePos[0]);
+    //         Obj_magazine.transform.localPosition = Vector3.zero;
+    //         Obj_magazine.transform.localRotation = Quaternion.identity;
+    //     }
+    // }
 
-    public void MagazinAppear()
-    {
-        Obj_magazine.SetActive(true);
-    }
+    // public void MagazinAppear()
+    // {
+    //     Obj_magazine.SetActive(true);
+    // }
     public void SniperIdle()
     {
         if (ac != null)
             ac.lerpTarget = 1.0f;
     }
-    public void CreateGunFire()
+    public void CreatePunchFire()
     {
         if (VFX_Adela_gunFire != null)
         {
-            GameObject vfx = Instantiate(VFX_Adela_gunFire, muzzle[0].transform.position, transform.rotation) as GameObject;
-            vfx.transform.SetParent(muzzle[0].transform);
+            GameObject vfx = Instantiate(VFX_Adela_gunFire, Punches[0].transform.position, transform.rotation) as GameObject;
+            vfx.transform.SetParent(Punches[0].transform);
         }
         SoundManager.Instance.PlayEffectSound(gunFire);
     }
@@ -320,18 +364,17 @@ public class DoubleGunController : ICareerController
         if (stream.IsWriting)
         {
             // We own this player: send the others our data
-            stream.SendNext(Beam[0].activeSelf);
-            stream.SendNext(Beam[1].activeSelf);
+            stream.SendNext(Punches[0].transform.position);
+            stream.SendNext(Punches[1].transform.position);
         }
         else
         {
             // Network player, receive data
-            this.Beam[0].SetActive((bool)stream.ReceiveNext());
-            this.Beam[1].SetActive((bool)stream.ReceiveNext());
+            this.Punches[0].transform.position = (Vector3)stream.ReceiveNext();
+            this.Punches[1].transform.position = (Vector3)stream.ReceiveNext();
         }
     }
     #endregion
-
 
 
     #region other method
