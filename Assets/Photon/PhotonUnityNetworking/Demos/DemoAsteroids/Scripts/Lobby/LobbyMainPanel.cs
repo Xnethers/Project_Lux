f8f783388a73a3399f58cf26ee0fd5bb5629b2f3
ams901026 <ams901026@gmail.com>
2019-12-14 18:01:46 +0800

    修正大叔吸血(無法傳DamageData問題，傳Player)

--- Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/Lobby/LobbyMainPanel.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/Lobby/LobbyMainPanel.cs
@@ -334,7 +334,7 @@ namespace Photon.Pun.Demo.Asteroids
 
             byte maxPlayers;
             byte.TryParse(MaxPlayersInputField.text, out maxPlayers);
-            maxPlayers = (byte)Mathf.Clamp(maxPlayers, 2, 8);
+            maxPlayers = (byte)Mathf.Clamp(maxPlayers, 4, 8);
 
             RoomOptions options = new RoomOptions { MaxPlayers = maxPlayers };
 
