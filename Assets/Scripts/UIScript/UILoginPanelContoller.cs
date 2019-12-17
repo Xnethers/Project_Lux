322922ec24cea94f9a3e3934aa02008a50252898
Xnethers <z44440000z@gmail.com>
2019-12-17 15:07:10 +0800

    套用UI中

--- Assets/Scripts/UIScript/UILoginPanelContoller.cs
+++ Assets/Scripts/UIScript/UILoginPanelContoller.cs
@@ -17,7 +17,7 @@ namespace UITween
         public RectTransform LoginPanel;
         public Vector2 StretchSize;
         public float StretchDuration;
-        public Text loginText;
+        //public Text loginText;
         private string logintextstring;
         public InputField playerNameInput;
         public Button loginButton;
@@ -29,8 +29,9 @@ namespace UITween
         // Use this for initialization
         void Start()
         {
-            logintextstring = loginText.text;
-            loginText.text = "";
+            LoginPanel.sizeDelta = Vector2.one;
+            // logintextstring = loginText.text;
+            // loginText.text = "";
         }
         void Update()
         {
@@ -53,21 +54,19 @@ namespace UITween
             // PanelHorizontalStretch(LoginPanel, StretchSize, 1);
             LoginPanel.gameObject.SetActive(true);
             Sequence mySequence = DOTween.Sequence();
-            Tweener scale1 = LoginPanel.DOSizeDelta(new Vector2(StretchSize.x, 1), StretchDuration).Pause();
+            Tweener scale1 = LoginPanel.DOSizeDelta(new Vector2(StretchSize.x, 10), StretchDuration).Pause();
             Tweener scale2 = LoginPanel.DOSizeDelta(StretchSize, StretchDuration).Pause();
-            Tweener showtext = loginText.DOText(logintextstring, StretchDuration).Pause();
-            mySequence.Append(scale1.Play()).Append(scale2.Play()).Append(showtext.Play());
+            //Tweener showtext = loginText.DOText(logintextstring, StretchDuration).Pause();
+            mySequence.Append(scale1.Play()).Append(scale2.Play());//.Append(showtext.Play());
         }
 
         public void RevokeLoginPanel()//取消登入
         {
             Sequence mySequence = DOTween.Sequence();
-            Tweener scale1 = LoginPanel.DOSizeDelta(new Vector2(LoginPanel.sizeDelta.x, 1), StretchDuration).Pause();
+            Tweener scale1 = LoginPanel.DOSizeDelta(new Vector2(LoginPanel.sizeDelta.x, 10), StretchDuration).Pause();
             Tweener scale2 = LoginPanel.DOSizeDelta(Vector2.one, StretchDuration).Pause().OnComplete(() => { lobbyMainPanel.LoginPanel.SetActive(false); });
-            Tweener cleartext = loginText.DOText("", StretchDuration);
-            mySequence.Append(scale1.Play()).Append(scale2.Play()).Append(cleartext);
-            // Tweener showtext = loginText.DOText(logintextstring, 1).Pause();
-            // PanelHorizontalStretch(LoginPanel, new Vector2(1, 1), 1);
+            //Tweener cleartext = loginText.DOText("", StretchDuration);
+            mySequence.Append(scale1.Play()).Append(scale2.Play());//.Append(cleartext);
         }
     }
 }
