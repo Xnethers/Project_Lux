ce04a69696610cf67d46b112044dd2a6f62a4582
Xnethers <z44440000z@gmail.com>
2019-12-17 13:59:10 +0800

    匯入LoginPanel，SelectionPanel，CreatRoomPanel，NoviceTeachingPanel的UI

--- Assets/Scripts/UIScript/UIAnimationContoller.cs
+++ Assets/Scripts/UIScript/UIAnimationContoller.cs
@@ -9,8 +9,13 @@ namespace UITween
 {
     public class UIAnimationContoller : MonoBehaviour
     {
+        public int level = 0;
+
+        private bool mouseStay;
         public RectTransform ThisButton;
         public Image ButtonImage;
+        public Sprite SelectSprite;
+        private Sprite originSprite;
         public Color color;
         private Color originColor;
         public float duration;
@@ -22,27 +27,50 @@ namespace UITween
         {
             originScale = ThisButton.localScale;
             originColor = ButtonImage.color;
+            originSprite = ButtonImage.sprite;
         }
 
-        public void ButtonScaleEnter(RectTransform rect, Vector3 endScale, float duration)
-        { ThisButton.DOScale(endScale, duration); }
-        public void ButtonScaleExit()
+        void Update()
         {
-            EventTrigger.Entry entry = new EventTrigger.Entry();
-            entry.eventID = EventTriggerType.Select;
-            if (entry.eventID == EventTriggerType.Select)
-            { ThisButton.DOScale(originScale, duration); }
+            if (Global.Level != level)
+            {ButtonImage.sprite = originSprite; }
+            else
+            {
+                ButtonImage.color = color;
+                ButtonImage.sprite = SelectSprite;
+            }
+        }
+
+        public void Enter()
+        { ButtonImage.DOColor(color, duration); }
+        public void Exit()
+        {
+            ButtonImage.DOColor(originColor, duration);
+        }
+
+        public void Click()
+        {
+            if (Global.Level != level)
+            { ButtonImage.sprite = SelectSprite; }
         }
 
         public void ColorChange()
         {
             ButtonImage.DOColor(color, duration);
         }
-        public void ColorChangeBack()
+        public void ColorChangeBack(int level)
         {
-            ButtonImage.DOColor(originColor, duration);
+            if (Global.Level != level)
+            { ButtonImage.DOColor(originColor, duration); }
         }
         public void SelectScale()
         { ThisButton.localScale = endScale; }
+        public void ImageChange(int level)
+        {
+            if (Global.Level == level)
+            { ButtonImage.sprite = SelectSprite; }
+            else
+            { ButtonImage.sprite = originSprite; }
+        }
     }
 }
